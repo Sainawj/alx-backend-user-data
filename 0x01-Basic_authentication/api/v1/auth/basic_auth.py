1ed5146a39a281ce9be578143b290ff7b8b774fc
Jonathan Saina <51122713+Sainawj@users.noreply.github.com>
2024-11-12 17:18:54 +0300

    first commit

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module for Basic API authentication."""
+from api.v1.auth.auth import Auth
+
+class BasicAuth(Auth):
+    """BasicAuth class inherits from Auth. Currently, it is empty."""
+    pass
