a95c570211ab2a1729fdade9d3c1068403ebbc8f
Jonathan Saina <51122713+Sainawj@users.noreply.github.com>
2024-11-12 17:25:07 +0300

    Update basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Module for Basic API authentication."""
 from api.v1.auth.auth import Auth
+import base64
 
 class BasicAuth(Auth):
     """BasicAuth class inherits from Auth."""
@@ -27,3 +28,27 @@ class BasicAuth(Auth):
         
         # Return the Base64 part after 'Basic ' (i.e., the part after the space)
         return authorization_header.split("Basic ")[1]
+    
+    
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """
+        Decodes the Base64 authorization header.
+
+        Args:
+            base64_authorization_header (str): The Base64 string to decode.
+
+        Returns:
+            str: The decoded string in UTF-8, or None if invalid.
+        """
+        if base64_authorization_header is None:
+            return None
+        
+        if not isinstance(base64_authorization_header, str):
+            return None
+        
+        try:
+            # Decode the Base64 string and return the result as a UTF-8 string
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
+        except (base64.binascii.Error, UnicodeDecodeError):
+            return None
