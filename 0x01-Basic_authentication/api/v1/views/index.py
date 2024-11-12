dba74b83b54407b3af04c66a3e14275b8ae5b0e2
Jonathan Saina <51122713+Sainawj@users.noreply.github.com>
2024-11-12 16:31:39 +0300

    Update index.py

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -1,6 +1,5 @@
-
 #!/usr/bin/env python3
-""" Module of index views
+"""Module of Index views.
 """
 from flask import jsonify, abort
 from api.v1.views import app_views
@@ -8,18 +7,18 @@ from api.v1.views import app_views
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
-    """ GET /api/v1/status
-    Return:
-      - the status of the API
+    """GET /api/v1/status
+    Returns:
+      - the status of the API.
     """
     return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
-    """ GET /api/v1/stats
-    Return:
-      - the number of each objects
+    """GET /api/v1/stats
+    Returns:
+      - the number of each object type.
     """
     from models.user import User
     stats = {}
@@ -28,18 +27,9 @@ def stats() -> str:
 
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
-def abort_401() -> str:
-    """ GET /api/v1/unauthorized
-    Return:
-      - test unauthorized endpoint
+def unauthorized() -> None:
+    """GET /api/v1/unauthorized
+    Raises:
+      - 401 Unauthorized error.
     """
     abort(401)
-
-
-@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
-def abort_403() -> str:
-    """ GET /api/v1/forbidden
-    Return:
-      - test forbidden endpoint
-    """
-    abort(403)
