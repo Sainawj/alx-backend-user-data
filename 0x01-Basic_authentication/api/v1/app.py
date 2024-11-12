b1c1a05e01868a5db302e24c0a84d640a1e89167
Jonathan Saina <51122713+Sainawj@users.noreply.github.com>
2024-11-12 17:05:10 +0300

    Update app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,7 +7,6 @@ from os import getenv
 
 from api.v1.auth.auth import Auth  # Import the Auth class
 
-
 app = Flask(__name__)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
@@ -17,7 +16,6 @@ auth_type = getenv("AUTH_TYPE", None)
 
 if auth_type == "auth":
     auth = Auth()
-
 
 @app.before_request
 def before_request():
@@ -36,28 +34,25 @@ def before_request():
 
     # Check if the request has an Authorization header
     if auth.authorization_header(request) is None:
-        abort(401, description="Unauthorized")
+        abort(401)
 
     # Check if the current user is authenticated
     if auth.current_user(request) is None:
-        abort(403, description="Forbidden")
+        abort(403)
 
 # Endpoint for checking API status
-
 @app.route('/api/v1/status/', methods=['GET'])
 def status():
     """Returns the status of the API."""
     return jsonify({"status": "OK"})
 
 # Unauthorized error handler
-
 @app.errorhandler(401)
 def unauthorized_error(error):
     """Handles 401 Unauthorized errors."""
     return jsonify({"error": "Unauthorized"}), 401
 
 # Forbidden error handler
-
 @app.errorhandler(403)
 def forbidden_error(error):
     """Handles 403 Forbidden errors."""
