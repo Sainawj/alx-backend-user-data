c18ff4476368fd1c70402912bc498b351327f1d1
Jonathan Saina <51122713+Sainawj@users.noreply.github.com>
2024-11-14 21:31:15 +0300

    Update app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -48,18 +48,21 @@ def before_request():
 
     # List of routes that do not require authentication
     excluded_paths = [
-            '/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/'
-            ]
+        '/api/v1/status/', 
+        '/api/v1/unauthorized/', 
+        '/api/v1/forbidden/', 
+        '/api/v1/auth_session/login/'  # Exclude this path from authentication
+    ]
 
     # If the path requires authentication
     if auth.require_auth(request.path, excluded_paths):
-        # Check if Authorization header is present
-        if auth.authorization_header(request) is None:
+        # Check if Authorization header and session cookie are both None
+        if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
             abort(401)  # Unauthorized error
 
         # Set the current_user to request
         request.current_user = auth.current_user(request)
-        
+
         # Check if the current user exists
         if request.current_user is None:
             abort(403)  # Forbidden error
