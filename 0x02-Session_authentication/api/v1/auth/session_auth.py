0980a084e67861807c8575c4bc9b366fe4555a23
Jonathan Saina <51122713+Sainawj@users.noreply.github.com>
2024-11-14 21:14:56 +0300

    Create session_auth.py

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+# api/v1/auth/session_auth.py
+
+from api.v1.auth.auth import Auth
+
+class SessionAuth(Auth):
+    """SessionAuth class for session-based authentication.
+    This is currently an empty class that inherits from Auth.
+    """
+    pass
